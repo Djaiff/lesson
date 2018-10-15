b322288fe3cabd82aefe793d7486abfd6e2b10fa
Jeffery Petit <petit.jeffery@gmail.com>
2018-10-15 16:15:03 +0200

    new review for the PDF summary
    This commit add several features to the PDF summary. It is knwo dependant on tex scripts and tex bibliography. Moreover, I reformat the sections so that they look more fluents for the reader. Finally, I updated some sentences and reformart some figures.

--- shiny/shinyApps/debug/server.r
+++ shiny/shinyApps/debug/server.r
@@ -1,12 +1,10 @@
 library(shiny)
-data(iris)
-
 # # open browser on error
 # options(shiny.error = browser)
 # # options(shiny.error = NULL)
 # 
 # # enabled reactlog
- # options(shiny.reactlog =TRUE)
+# options(shiny.reactlog =TRUE)
 # 
 # # view all communication between client and server
 # options(shiny.trace = FALSE)
@@ -19,21 +17,23 @@ data(iris)
 
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
-    if(input$go_graph > 0){
-      isolate({
-        x <- iris[, input$variable]
-        
-        # a print to console, visibled also in shiny-server logs
-        # cat(file=stderr(), class(x), "\n")
-        
-        # launch browser
-        # browser()
-        
-        # launch shiny.reactlog
-        # showReactLog()
-        
-        hist(x)
-      }) 
-    }
+    input$go_graph
+    validate(
+      need(input$variable != "Species", "Need a numeric column")
+    )
+    isolate({
+      x <- iris[, input$variable]
+      
+      # a print to console, visibled also in shiny-server logs
+      # cat(file=stderr(), class(x), "\n")
+      
+      # launch browser
+      # browser()
+      
+      # launch shiny.reactlog
+      # showReactLog()
+      
+      hist(x)
+    })
   })
 })
