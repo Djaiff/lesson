853dd97597cc082a56b1f1fa02cb763395800e1b
Jeffery Petit <petit.jeffery@gmail.com>
2018-10-19 06:33:13 +0200

    Reformat structurte
    Updated files and reformated structure

---
+++ shiny/TP/Corrections/Graphiques_reactivite/server.R
@@ -0,0 +1,49 @@
+
+shinyServer(function(input, output, session) { # session pour observe / update
+  
+  # donnees pour les graphiques
+  current_data <- reactive({
+    faithful[, input$variable]
+  })
+  
+  output$distPlot <- renderAmCharts({
+    input$goButton
+    isolate({
+      # generate bins based on input$bins from ui.R
+      bins <- seq(min(current_data()), max(current_data()), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      amHist(current_data(), control_hist = list(breaks = round(bins, 2)), col = input$couleur,
+             main  = input$titre, zoom = TRUE, export = TRUE) 
+    })
+  })
+  
+  # output pour afficher le nombre de classes
+  output$nbbins <- renderText({
+    paste0("Cet histogramme a été réalisé en utilisant ", input$bins, " classes.")
+  })
+  
+  # output pour afficher les données
+  output$view_data <- renderDataTable({
+    faithful
+  })
+  
+  # output pour afficher le summary des données
+  output$summary_data <- renderPrint({
+    summary(faithful)
+  })
+  
+  # rajout du boxplot
+  output$distPlot2 <- renderAmCharts({
+    input$goButton
+
+    # avec la même couleur que l'histogramme
+    amBoxplot(isolate(current_data()), col = isolate(input$couleur), export = TRUE)
+  })
+  
+  # observe pour afficher toujours l'histogramme en premier a la validation
+  observe({
+    input$goButton
+    updateTabsetPanel(session, inputId = "graph_panel", selected = "Histogramme")
+  })
+})
